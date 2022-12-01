765fbb75d3fe9731fc058a7a994c9a0b5876f412
Radstrom <mattias.radstrom@forefront.se>
2022-12-01 06:19:08 +0100

    Initial solve

---
+++ AdventOfCode2022.Day1.Tests/Tests.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+
+namespace AdventOfCode2022.Day1.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var text = File.ReadAllText("TestData");
+        text.Should().NotBeEmpty();
+        First.Solve(text).Should().Be(24000);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var text = File.ReadAllText("Data");
+        text.Should().NotBeEmpty();
+        First.Solve(text).Should().Be(70720);
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var text = File.ReadAllText("TestData");
+        text.Should().NotBeEmpty();
+        Second.Solve(text).Should().Be(45000);
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var text = File.ReadAllText("Data");
+        text.Should().NotBeEmpty();
+        Second.Solve(text).Should().Be(207148);
+    }
+}
---
+++ AdventOfCode2022.Day1/First.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2022.Day1;
+
+public static class First
+{
+    public static int Solve(string input)
+    {
+        var split = input.Split("\n\n");
+
+        var biggest = 0;
+        foreach (var elf in split)
+        {
+            var totaltCals = 0;
+            foreach (var cals in elf.Split("\n"))
+            {
+                totaltCals += Int32.Parse(cals);
+            }
+
+            if (totaltCals > biggest)
+            {
+                biggest = totaltCals;
+            }
+        }
+        
+        return biggest;
+    }
+}
